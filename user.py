1cc5b347cf8ff5658e259f0ab7b4374c3fb2c99d
Shamim <shamimmaloba@gmail.com>
2022-01-23 22:31:51 +0300

    added imports

---
+++ user.py
@@ -0,0 +1,19 @@
+import random
+import string 
+
+
+class user:
+
+    userslist = []
+def _init_(self,firstname,lastname,username,password):
+    self.firstname = firstname
+    self.lastname = lastname
+    self.username = username
+    self.password = password
+
+
+def save_user(self):
+    user.userslist.append(self)
+
+# def delete_user(self):
+
