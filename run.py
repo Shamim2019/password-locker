db4dcd763553611bc5d37940fae7afd9de8c0dbe
Shamim <shamimmaloba@gmail.com>
2022-01-25 11:15:11 +0300

    createde useraccount

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
 
 import string
-from random import 
+# from random import 
+import random
 from user import User
 from user import credentials
+
+
+def create_user(firstname,lastname,username,userpassword):
+    newuser= User(firstname,lastname,username,userpassword)
+    return newuser
+
+def save_user(user):
+    user.delete-user()
+
+def find_user(number):
+    return User.find_by_number(number)
+
+def display_users():
+    return User.display_users()
+
+def create_account(accountusername,accountname,accountpassword):
+    newaccount =credentials(accountusername,accountname,accountpassword)
